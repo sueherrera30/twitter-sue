776bb4222a7b0c4e97c2e0db796ab88d42aa067d
sue herrera <sueherrera30@gmail.com>
2017-04-04 00:42:46 -0500

    version fallida ninja

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 
 var i = 0;
+var clicks = 0;
 var boton = document.getElementById('boton');
 boton.addEventListener("click",agregarTweet);
 
@@ -17,9 +18,30 @@ function agregarTweet(){
    function contadorLetritas(){
     var contador= document.getElementById('contador');
     var texto = document.getElementById('areaTexto');
+
     i++;
     if( i >= 140){
       alert("exediste el numero de palabras disponibles");
     }
-     contador.innerHTML = "has usado " + i + " de 140 palabras";
+     contador.innerHTML = "has usado " + i + "  /140";
+
 }
+
+document.addEventListener("click",contadorClicks);
+
+function contadorClicks(){
+var contadorAlmacen = document.getElementById('contadorClicks');
+clicks++;
+contadorAlmacen.innerHTML = "Van "+ clicks +" clicks dados";
+}
+//    var cajaTexto = document.getElementById('areaTexto');
+//     cajaTexto.stopPropagation();
+//     })
+  //   function clickInput(){
+  //  var input = document.getElementById('autor');
+  //  event.stopPropagation();
+  //  }
+  //  function clickBoton(){
+  // var boton = document.getElementById('boton');
+  // event.stopPropagation();
+  //  }
